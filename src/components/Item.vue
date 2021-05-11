4adbd60fb5bea1ea92f141af1d5bc5a35ce6bd74
ORBIS\akmati <a.akmal@hbksaar.de>
2021-05-12 01:33:44 +0200

    added project files

---
+++ src/components/Item.vue
@@ -0,0 +1,17 @@
+<template>
+      <li>
+         {{item.name}}
+         <button @click="delMethod(index)"   title="Delete">delete</button>
+       </li>
+</template>
+
+<script>
+export default {
+  name: 'ListOfItems',
+  props: {
+    delMethod: {type: Function},
+    item: {type: Object},
+    index: {type: Number},
+  }
+}
+</script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>Item list component</h1>
+    <ul>
+        <Item v-for="(item, index) in itemList" :key="item.id" :delMethod="del" :index="index" :item="item"/>
+    </ul>
+
+    <!-- add items in a list -->
+    <input type="text" v-model="inputValue" id="addValue" class="form-control" />
+    <button value="{{$id}}" @click="add()"   title="Add">add</button><br>
+</template>
+
+<script>
+import lists from "./lists.json";
+import Item from './Item.vue';
+export default {
+  name: 'ItemList',
+  components: {
+    Item
+  },
+
+    data() {
+        return {
+            itemList: lists
+        };
+    },
+    methods: {
+        add() {
+            this.itemList.push({'id':(this.itemList.length) + 1,'name':this.inputValue});
+            this.inputValue = '';
+        },
+        del(index) {
+            this.itemList.splice(index, 1);
+        }
+    }
+}
+</script>
